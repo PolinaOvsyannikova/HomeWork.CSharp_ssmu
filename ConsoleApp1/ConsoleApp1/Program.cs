cc1ffad385dfd2968c37f4053d090023428daabf
Polina <polly.tomsk@gmail.com>
2023-03-11 16:25:47 +0700

    Created a program and added Homework1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите Ваше имя:");
+        var name = Console.ReadLine();
+        Console.WriteLine($"{name}, привет!");
+    }
+}
+
