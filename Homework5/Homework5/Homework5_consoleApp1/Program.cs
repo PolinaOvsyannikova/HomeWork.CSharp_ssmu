3794d0c9695f6e8940673a11e708394cb6c2f8d9
Polina <polly.tomsk@gmail.com>
2023-04-09 22:53:04 +0700

    Added homework  5

---
+++ Homework5/Homework5/Homework5_consoleApp1/Program.cs
@@ -0,0 +1,90 @@
+﻿using Common;
+using System.IO.Compression;
+using System.Text;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        const string _zipPath = "D://test.zip";
+
+        const string _folderPath = "Папка";
+
+        const string _filePath = "D:\\C#\\HomeWork.CSharp_ssmu.git\\Homework5\\Homework5\\Homework5_consoleApp1\\bin\\Debug\\net6.0\\fileInfo.csv";
+
+        try
+        {
+            if (!Directory.Exists(_folderPath))
+            {
+                Directory.CreateDirectory(_folderPath);
+                ZipFile.ExtractToDirectory($"{_zipPath}", $"{_folderPath}");
+            }
+        }
+      catch (Exception ex) 
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        var fileData = ShowDirectoryAndFile(_folderPath);
+
+        CreateAndWriteCSVFile(_filePath, fileData);
+
+        if (Directory.Exists(_folderPath))
+        {
+            Directory.Delete(_folderPath, true);
+        }
+
+        string path2 = Environment.ExpandEnvironmentVariables("%appData%//Lesson12Homework.txt");
+
+        File.Create(path2).Close();
+        
+        using var streamWriter = new StreamWriter(path2);
+
+        streamWriter.Write(_filePath);
+    }
+    static List<Record> ShowDirectoryAndFile(string path)
+    {
+        List<Record> records = new List<Record>();
+        foreach (var element in Directory.GetDirectories(path))
+        {
+            FileInfo fileInfo = new FileInfo(element);
+            records.Add(new Record("Folder",fileInfo.Name, fileInfo.LastWriteTime));
+        }
+
+        foreach (var element in Directory.GetFiles(path))
+        {
+            FileInfo fileInfo = new FileInfo(element);
+            records.Add(new Record("File", fileInfo.Name, fileInfo.LastWriteTime));
+        }
+        return records;
+    }
+    static void CreateAndWriteCSVFile(string path, List<Record> records)
+    {
+        File.Create(path).Dispose();
+
+        var stringOfRecords = ConvertListRecordsToString(records);
+
+        File.AppendAllText(path, stringOfRecords);
+    }
+    static string ConvertListRecordsToString(List<Record> record)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (var recordItem in record)
+        {
+            stringBuilder.Append(recordItem.Type);
+            stringBuilder.Append(";");
+            stringBuilder.Append(recordItem.Name);
+            stringBuilder.Append(";");
+            stringBuilder.Append(recordItem.LastWriteTime);
+            stringBuilder.Append("\n");
+        }
+        return stringBuilder.ToString();
+    }
+}
+
+
+
+ 
+
+
---
+++ Homework5/Homework5/Homework5_consoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using Common;
+
+string path = Environment.ExpandEnvironmentVariables("%appData%//Lesson12Homework.txt");
+using var pathReader = new StreamReader(path);
+
+var csvPath = pathReader.ReadToEnd();
+
+using var csvReader = new StreamReader(csvPath);
+var csvData = csvReader.ReadToEnd();
+
+List<Record> records = new List<Record>();
+
+try
+{
+    string[] stringRecords = csvData.Split('\n');
+    foreach (string stringRecord in stringRecords)
+    {
+        if (!string.IsNullOrWhiteSpace(stringRecord))
+        {
+            string[] innerData = stringRecord.Split(';');
+            records.Add(new Record(innerData[0], innerData[1], DateTime.Parse(innerData[2])));
+        }
+    }
+}
+catch
+{
+    Console.WriteLine("Файл повреждён");
+}
+
+records.OrderBy(x=>x.LastWriteTime);
+
+foreach(var record in records)
+{
+    Console.WriteLine($"Тип файла - {record.Type}");
+    Console.WriteLine($"Имя файла - {record.Name}");
+    Console.WriteLine($"Время последнего изменения файла - {record.LastWriteTime}");
+    Console.WriteLine();
+}
